21c973bbeaea4c7287a2fb5345a3a4137a2613c8
kallamvishnupriya <vishnupriyakallam@gmail.com>
2026-05-19 15:59:20 +0530

    connect render postgres database

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn event_management.wsgi
+web: gunicorn event_management.wsgi:application
--- event_management/settings.py
+++ event_management/settings.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 from decouple import config
-import pymysql
-
-pymysql.install_as_MySQLdb()
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -11,9 +9,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ─────────────────────────────
 SECRET_KEY = config("SECRET_KEY")
 
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = config(
+    "DEBUG",
+    default="False",
+    cast=lambda v: v.lower() == "true"
+)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*").split(",")
+    "ALLOWED_HOSTS",
+    default="*"
+).split(",")
 
 # ─────────────────────────────
 # APPS
@@ -44,7 +49,6 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
 
     'django.contrib.sessions.middleware.SessionMiddleware',
-
     'corsheaders.middleware.CorsMiddleware',
 
     'django.middleware.common.CommonMiddleware',
@@ -78,20 +82,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'event_management.wsgi.application'
 
 # ─────────────────────────────
-# DATABASE (MySQL - Railway)
+# DATABASE (RENDER POSTGRESQL)
 # ─────────────────────────────
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST"),
-        "PORT": config("DB_PORT", default="3306"),
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
+    'default': dj_database_url.config(
+        default=config("DATABASE_URL")
+    )
 }
 
 # ─────────────────────────────
@@ -117,7 +113,7 @@ USE_I18N = True
 USE_TZ = True
 
 # ─────────────────────────────
-# STATIC FILES (Railway + WhiteNoise)
+# STATIC FILES
 # ─────────────────────────────
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
